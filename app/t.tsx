2d9776378fadb6dc163d69e0ca4ee611133b0342
niloofar yousefiafrouz <niloofar.yousefiafrouz@dci-student.org>
2025-12-24 21:08:43 +0100

    1

--- app/t.tsx
+++ app/t.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const t = () => {
   return (
     <div>
-      Nextora Geld git Geld Geld  Geld Geld  Geld viel viel viel 
+      Nextora Geld git Geld Geld  Geld Geld  Geld viel viel viel Geld
     </div>
   )
 }
